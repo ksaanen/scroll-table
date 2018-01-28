21faa30c0e80b7f4eeeb171c90ea66122ac47075
Kevin <kevinsa@protonmail.com>
2018-01-28 22:13:54 +0100

    update directive

--- frontend/js/scroll-table.v2.directive.js
+++ frontend/js/scroll-table.v2.directive.js
@@ -6,98 +6,98 @@
   directive.$inject = ['$window'];
   function directive($window) {
 
-    let directive = {
-      bindToController: true,
-      controller: controller,
-      controllerAs: '$ctrl',
-      link: link,
-      restrict: 'A',
-      scope: {}
-    };
-    return directive;
+    return {
+      transclude: true,
+      restrict: 'E',
+      scope: {
+        rows: '=watch',
+        sortFn: '='
+      },
+      template: '<div class="scrollableContainer">' +
+                  '<div class="headerSpacer"></div>' +
+                  '<div class="scrollArea" ng-transclude></div>' +
+                '</div>',
+      controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
 
-    function link(scope, element, attrs) {
+    let $table = angular.element(element)[0];
+    let $thead = $table.querySelector('thead');
+    let $tbody = $table.querySelector('tbody');
+    let $tfoot = $table.querySelector('tfoot');
 
-      let $table = angular.element(element)[0];
-      let $thead = $table.querySelector('thead');
-      let $tbody = $table.querySelector('tbody');
-      let $tfoot = $table.querySelector('tfoot');
+    let $thead_cells = $thead.rows[0].children;
+    let $tbody_cells = $tbody.rows[0].children;
+    let $tfoot_cells = $tfoot.rows[0].children;
 
-      let $thead_cells = $thead.rows[0].children;
-      let $tbody_cells = $tbody.rows[0].children;
-      let $tfoot_cells = $tfoot.rows[0].children;
+    let $fixHeader = attrs.hasOwnProperty('fixedHeader');
+    let $fixFooter = attrs.hasOwnProperty('fixedFooter');
 
-      let $fixHeader = attrs.hasOwnProperty('fixedHeader');
-      let $fixFooter = attrs.hasOwnProperty('fixedFooter');
-
-      function apply() {
-        for (let i = 0; i < $tbody_cells.length; i++) {
-          let $max_value = Math.max($thead_cells[i].offsetWidth, $tbody_cells[i].offsetWidth, $tfoot_cells[i].offsetWidth);
-          if ($fixHeader) {
-            angular.element($thead_cells[i]).css('width', $max_value+'px');
-            angular.element($thead_cells[i]).css('min-width', $max_value+'px');
-          }
-          if ($fixFooter) {
-            angular.element($tfoot_cells[i]).css('width', $max_value+'px');
-            angular.element($tfoot_cells[i]).css('min-width', $max_value+'px');
-          }
-          angular.element($tbody_cells[i]).css('width', $max_value+'px');
-          angular.element($tbody_cells[i]).css('min-width', $max_value+'px');
+    function apply() {
+      for (let i = 0; i < $tbody_cells.length; i++) {
+        let $max_value = Math.max($thead_cells[i].clientWidth, $tbody_cells[i].clientWidth, $tfoot_cells[i].clientWidth);
+        if ($fixHeader) {
+          angular.element($thead_cells[i]).css('width', $max_value+'px');
+          angular.element($thead_cells[i]).css('min-width', $max_value+'px');
         }
-      }
-      
-      function position() {
-        if ($fixHeader && $thead) {
-          element.css('padding-top', $thead.offsetHeight+'px');
-          angular.element($thead).css('top', $table.scrollTop+'px');
+        if ($fixFooter) {
+          angular.element($tfoot_cells[i]).css('width', $max_value+'px');
+          angular.element($tfoot_cells[i]).css('min-width', $max_value+'px');
         }
-        if ($fixFooter && $tfoot) {
-          element.css('padding-bottom', $tfoot.offsetHeight+'px');
-          angular.element($tfoot).css('top', ($table.scrollTop + ($table.clientHeight - $tfoot.offsetHeight))+'px');
-        }
+        angular.element($tbody_cells[i]).css('width', $max_value+'px');
+        angular.element($tbody_cells[i]).css('min-width', $max_value+'px');
       }
-
-      function reset() {
-        for (let i = 0; i < $tbody_cells.length; i++) {
-          if ($fixHeader) {
-            angular.element($thead_cells[i]).css('width',null);
-            angular.element($thead_cells[i]).css('min-width',null);
-          }
-          if ($fixFooter) {
-            angular.element($tfoot_cells[i]).css('width',null);
-            angular.element($tfoot_cells[i]).css('min-width',null);
-          }
-          angular.element($tbody_cells[i]).css('width',null);
-          angular.element($tbody_cells[i]).css('min-width',null);
-        }
+    }
+    
+    function position() {
+      if ($fixHeader && $thead) {
+        element.css('padding-top', $thead.offsetHeight+'px');
+        angular.element($thead).css('top', $table.scrollTop+'px');
       }
-      
-      function init() {
-        apply();
-        position();
+      if ($fixFooter && $tfoot) {
+        element.css('padding-bottom', $tfoot.offsetHeight+'px');
+        angular.element($tfoot).css('top', ($table.scrollTop + ($table.clientHeight - $tfoot.offsetHeight))+'px');
       }
-
-      $table.addEventListener('scroll', function() {
-        position();
-      });
-
-      $window.addEventListener('resize', function(){
-        reset();
-        init();
-      });
-
-      init();
-
-      scope.$on('$destroy', function () {
-        $window.removeEventListener('resize');
-        element.remove();
-      });
     }
 
+    function reset() {
+      for (let i = 0; i < $tbody_cells.length; i++) {
+        if ($fixHeader) {
+          angular.element($thead_cells[i]).css('width',null);
+          angular.element($thead_cells[i]).css('min-width',null);
+        }
+        if ($fixFooter) {
+          angular.element($tfoot_cells[i]).css('width',null);
+          angular.element($tfoot_cells[i]).css('min-width',null);
+        }
+        angular.element($tbody_cells[i]).css('width',null);
+        angular.element($tbody_cells[i]).css('min-width',null);
+      }
+    }
+    
+    function init() {
+      apply();
+      position();
+    }
 
-  }
+    $table.addEventListener('scroll', function() {
+      position();
+    });
 
-  function controller () {
+    $window.addEventListener('resize', function(){
+      reset();
+      init();
+    });
+
+    init();
+
+    scope.$on('$destroy', function () {
+      $window.removeEventListener('resize');
+      element.remove();
+    });
+  }]
+
+  
+}     
+    
 
   }
 })();
