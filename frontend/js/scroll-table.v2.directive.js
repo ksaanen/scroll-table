db94c42b79b2808cd9d24eaa1f19f6e95a4cbacc
ksaanen <ksaanen@independer.nl>
2018-01-29 11:08:50 +0100

    simplified AngularJS code

--- frontend/js/scroll-table.v2.directive.js
+++ frontend/js/scroll-table.v2.directive.js
@@ -6,98 +6,83 @@
   directive.$inject = ['$window'];
   function directive($window) {
 
-    return {
-      transclude: true,
-      restrict: 'E',
-      scope: {
-        rows: '=watch',
-        sortFn: '='
-      },
-      template: '<div class="scrollableContainer">' +
-                  '<div class="headerSpacer"></div>' +
-                  '<div class="scrollArea" ng-transclude></div>' +
-                '</div>',
-      controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+    function link($scope, $element, $attrs) {
 
-    let $table = angular.element(element)[0];
-    let $thead = $table.querySelector('thead');
-    let $tbody = $table.querySelector('tbody');
-    let $tfoot = $table.querySelector('tfoot');
+      let $table = $element.find("table");
+      let $thead = $element.find("thead");
+      let $tbody = $table.find("tbody");
+      let $tfoot = $table.find("tfoot");
 
-    let $thead_cells = $thead.rows[0].children;
-    let $tbody_cells = $tbody.rows[0].children;
-    let $tfoot_cells = $tfoot.rows[0].children;
+      let $thead_cells = $thead.find("tr").children();
+      let $tbody_cells = $tbody.children().eq(0).children();
+      let $tfoot_cells = $tfoot.find("tr").children();
 
-    let $fixHeader = attrs.hasOwnProperty('fixedHeader');
-    let $fixFooter = attrs.hasOwnProperty('fixedFooter');
+      let $fixHeader = $attrs.hasOwnProperty('fixedHeader');
+      let $fixFooter = $attrs.hasOwnProperty('fixedFooter');
 
-    function apply() {
-      for (let i = 0; i < $tbody_cells.length; i++) {
-        let $max_value = Math.max($thead_cells[i].clientWidth, $tbody_cells[i].clientWidth, $tfoot_cells[i].clientWidth);
-        if ($fixHeader) {
-          angular.element($thead_cells[i]).css('width', $max_value+'px');
-          angular.element($thead_cells[i]).css('min-width', $max_value+'px');
+      function apply() {
+        for (let i = 0; i < $tbody_cells.length; i++) {
+          let $max_value = Math.max($thead_cells.eq(i).prop('offsetWidth'), $tbody_cells.eq(i).prop('offsetWidth'), $tfoot_cells.eq(i).prop('offsetWidth'));
+         
+          if ($fixHeader) {
+            $thead_cells.eq(i).css('width', $max_value + 'px');
+            $thead_cells.eq(i).css('min-width', $max_value + 'px');
+          }
+          if ($fixFooter) {
+            $tfoot_cells.eq(i).css('width', $max_value + 'px');
+            $tfoot_cells.eq(i).css('min-width', $max_value + 'px');
+          }
+          $tbody_cells.eq(i).css('width', $max_value + 'px');
+          $tbody_cells.eq(i).css('min-width', $max_value + 'px');
         }
-        if ($fixFooter) {
-          angular.element($tfoot_cells[i]).css('width', $max_value+'px');
-          angular.element($tfoot_cells[i]).css('min-width', $max_value+'px');
+      }
+      
+      function position() {
+        if ($fixHeader && $thead) {
+          $element.css('padding-top', $thead.prop('offsetHeight') + 'px');
+          $thead.css('top', $element.prop('scrollTop') + 'px');
         }
-        angular.element($tbody_cells[i]).css('width', $max_value+'px');
-        angular.element($tbody_cells[i]).css('min-width', $max_value+'px');
-      }
-    }
-    
-    function position() {
-      if ($fixHeader && $thead) {
-        element.css('padding-top', $thead.offsetHeight+'px');
-        angular.element($thead).css('top', $table.scrollTop+'px');
-      }
-      if ($fixFooter && $tfoot) {
-        element.css('padding-bottom', $tfoot.offsetHeight+'px');
-        angular.element($tfoot).css('top', ($table.scrollTop + ($table.clientHeight - $tfoot.offsetHeight))+'px');
-      }
-    }
-
-    function reset() {
-      for (let i = 0; i < $tbody_cells.length; i++) {
-        if ($fixHeader) {
-          angular.element($thead_cells[i]).css('width',null);
-          angular.element($thead_cells[i]).css('min-width',null);
+        if ($fixFooter && $tfoot) {
+          $element.css('padding-bottom', $tfoot.prop('offsetHeight') + 'px');
+          $tfoot.css('top', ($element.prop('scrollTop') + ($element.prop('clientHeight') - $tfoot.prop('offsetHeight'))) + 'px');
         }
-        if ($fixFooter) {
-          angular.element($tfoot_cells[i]).css('width',null);
-          angular.element($tfoot_cells[i]).css('min-width',null);
-        }
-        angular.element($tbody_cells[i]).css('width',null);
-        angular.element($tbody_cells[i]).css('min-width',null);
       }
-    }
-    
-    function init() {
-      apply();
-      position();
-    }
 
-    $table.addEventListener('scroll', function() {
-      position();
-    });
+      function reset() {
+        for (let i = 0; i < $tbody_cells.length; i++) {
+          if ($fixHeader) {
+            $thead_cells.eq(i).css('width', null);
+            $thead_cells.eq(i).css('min-width', null);
+          }
+          if ($fixFooter) {
+            $tfoot_cells.eq(i).css('width', null);
+            $tfoot_cells.eq(i).css('min-width', null);
+          }
+          $tbody_cells.eq(i).css('width', null);
+          $tbody_cells.eq(i).css('min-width', null);
+        }
+      }
+      
+      function init() {
+        apply();
+        position();
+      }
 
-    $window.addEventListener('resize', function(){
-      reset();
+      $element.bind('scroll', function(e) {
+        position();
+      });
+
+      angular.element($window).bind('resize', function(e){
+        reset();
+        init();
+      });
+
       init();
-    });
 
-    init();
-
-    scope.$on('$destroy', function () {
-      $window.removeEventListener('resize');
-      element.remove();
-    });
-  }]
-
-  
-}     
-    
-
+    }
+    return {
+      link: link,
+      scope: {}
+    }
   }
 })();
