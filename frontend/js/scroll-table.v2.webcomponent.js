0b427c0e7cfd9b41ab8b97d8048cd62278955acc
ksaanen <kevinsa@protonmail.com>
2018-02-06 11:53:30 +0100

    add initial webcomponents setup

---
+++ frontend/js/scroll-table.v2.webcomponent.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Webcomponents try-out as documented at:
+// https://developer.mozilla.org/en-US/docs/Web/Web_Components
+
+class scrollTable {
+
+  constructor() {
+
+    super(); // Always call super first in constructor
+
+  }
+}
+
+customElements.define('scroll-table', scrollTable);
